531098630e73432e3c82ee878fdfe94d20a5f0bf
Hans Surya Cendikia <kazama2510@gmail.com>
2023-03-16 10:01:30 +0700

    160323
    160323

--- src/Hans080323/GetInputFromKeyboard.java
+++ src/Hans080323/GetInputFromKeyboard.java
@@ -4,10 +4,21 @@
  */
 package Hans080323;
 
-/**
- *
- * @author ACER
- */
-public class GetInputFromKeyboard {
-    
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class GetInputFromKeyboard
+{
+    public static void main( String[] args ){
+        BufferedReader dataIn = new BufferedReader(new InputStreamReader( System.in) );
+            String name = "";
+                System.out.print("Please Enter Your Name:");
+try{
+name = dataIn.readLine();
+}catch( IOException e ){
+System.out.println("Error!");
+}
+System.out.println("Hello " + name +"!");
+}
 }
